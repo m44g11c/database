33c473a31fe98d573ef96a6dcb4c7892662678a1
m44g11c <abtoputet@tut.by>
2015-12-01 22:51:27 +0300

    fix

--- cfg/database.php
+++ cfg/database.php
@@ -1,15 +1,16 @@
 <?php
 
-	$host = 'localhost'; // хост
-	$dbname = 'database'; // название базы
-	$user = "root"; // логин пользователя
-	$password = ""; // пароль
+$host = 'localhost'; // хост
+$dbname = 'database'; // название базы
+$user = "root"; // логин пользователя
+$password = ""; // пароль
 
 
-	try {
-		$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-		} catch (PDOException $e) {
-	  	var_dump($e);
-	};
+try {
+	$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+	} catch (PDOException $e) {
+  	echo $e->getMessage();
+};
+
 
 ?>
