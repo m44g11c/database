7b4f78f3930ddb2e12f56941736785626da941b3
m44g11c <abtoputet@tut.by>
2015-12-01 15:34:03 +0300

    add login

--- registr.php
+++ registr.php
@@ -18,26 +18,45 @@ try {
 };
 
 
+// register
 
-
-if (isset($_POST[submit])) {
+if (isset($_POST['submit'])) {
 	
-	$name = $_POST[name];
-	$login = $_POST[login];
-	$mail = $_POST[mail];
-	$pass = $_POST[pass];
-	$rpass = $_POST[rpass];
+	$name = $_POST['name'];
+	$login = $_POST['login'];
+	$mail = $_POST['mail'];
+	$pass = $_POST['pass'];
+	$rpass = $_POST['rpass'];
 	if ($pass == $rpass) {
 		$pass = md5($pass);
 		$query = $db->prepare("INSERT INTO `users` (name, login, pass, email) VALUES('$name', '$login', '$pass', '$mail')");
+		$res = $query->execute();
+
 	} else { 
 
 		die("Passwords dont match!");
 
 	};
+
+// login
+
+if (isset($_POST['enter'])) {
+	$e_login = $_POST['e_login'];
+	$e_pass = md5($_POST['e_pass']);
+
+	$query = $db->prepare("SELECT * FROM `users` WHERE login = $e_login");
+	$res = $query->execute();
+	// $result = $res->fetchAll();
+	// print_r($result);
+}
+
+
 	
 
 };
+
+
+// SELECT * FROM users WHERE pass = $e_pass
 
 ?>
 
@@ -49,13 +68,20 @@ if (isset($_POST[submit])) {
 	<p><input type="text" name="mail"  placeholder="* Mail"></p>
 	<p><input type="password" name="pass"  placeholder="* Password"></p>
 	<p><input type="password" name="rpass"  placeholder="* Repeat password"></p>
-	<p><input type="submit" value="Click" name="submit"></p>
+	<p><input type="submit" value="Registration" name="submit"></p>
 </form>
 
 
-<?php
+<form method="post" action="registr.php">
 	
-	echo "$name $login $mail $pass $rpass";
+	<p><input type="text" name="e_login"  placeholder="* Login"></p>
+	<p><input type="password" name="e_pass"  placeholder="* Password"></p>
+	<p><input type="submit" value="Login" name="enter"></p>
+	
+</form>
+
+<?php
+
+print_r($res);
 
 ?>
-
